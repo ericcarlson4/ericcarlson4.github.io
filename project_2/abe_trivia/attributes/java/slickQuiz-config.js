e60209486af9c78fc99e5c6b9b1883d6536f5bbc
ecarlso4 <ecarlso4@nd.edu>
2018-10-20 12:57:44 -0400

    Quizes completed.

--- project_2/abe_trivia/attributes/java/slickQuiz-config.js
+++ project_2/abe_trivia/attributes/java/slickQuiz-config.js
@@ -5,68 +5,70 @@
 var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "main":    "<p>How much do you think you know about Abe Lincoln? Take this quiz to find out!</p>",
+        "results": "<h5>Learn More</h5><p>How did you do? If you want to learn more about Teddy, just click on the 'Learn More' button!</p>",
+        "level1":  "Abe Expert",
+        "level2":  "Abe Fan",
+        "level3":  "Not bad",
+        "level4":  "Getting there",
+        "level5":  "Give it another shot!" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "What kind of building was Abe Lincoln born in?",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "A hospital",      "correct": false},
+                {"option": "A mansion",     "correct": false},
+                {"option": "A house in the suburbs",      "correct": false},
+                {"option": "A log cabin",     "correct": true} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> Abe was born in a log cabin in Kentucky</p>",
+            "incorrect": "<p><span> Not quite.</span> He was born in a different kid of building.</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+            "q": "As a young man, what sport did Abe Lincoln take part in?",
             "a": [
-                {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
-                {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "baseball",               "correct": false},
+                {"option": "boxing","correct": true},
+                {"option": "cross-country",               "correct": false},
+                {"option": "football", "correct": false} // no comma here
             ],
             "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>Nice!</span> Abe was a boxing champion!</p>",
+            "incorrect": "<p><span>Hmmm.</span> Nope, that's not the right sport.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+            "q": "Abe lincoln debated this man about slavery, even before the civil war.",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "Fredrick Douglass",           "correct": false},
+                {"option": "Jim Crow'",                  "correct": false},
+                {"option": "Stephen Douglas",  "correct": true},
+                {"option": "Paul McCartney",          "correct": false} // no comma here
             ],
-            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+            "correct": "<p><span>Nailed it!</span>Abe's debate with Stephen Douglas was incredibly famous! Abe argued against slavery. </p>",
+            "incorrect": "<p><span>Not Quite.</span> Next time, I guess.</p>" // no comma here
         },
         { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+            "q": "Abe wrote this important document, attempting to free slaves in the South.",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "The Emancipation Proclamation",    "correct": true},
+                {"option": "The Emancipation Free-Slaves Declaration",     "correct": false},
+                {"option": "The 13th Ammendment",      "correct": false},
+                {"option": "The Lord of the Rings",   "correct": false} // no comma here
             ],
-            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+            "correct": "<p><span>Correct!</span> While the Emancipation Proclamation did not actually free the slaves in the South, it inspired many slaves to escape.</p>",
+            "incorrect": "<p><span>Oops.</span> Wrong one.</p>" // no comma here
         },
         { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+            "q": "Abraham Lincoln was assassinated by John Wilkes Booth in...",
             "a": [
-                {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "a car",    "correct": false},
+                {"option": "a theatre",     "correct": true},
+                {"option": "the White House",      "correct": false},
+                {"option": "a Civil War battle.",   "correct": false} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+            "correct": "<p><span>Correct.</span> Tragically, John Wilkes Booth was an actor of the theatre and shot Lincoln.</p>",
+            "incorrect": "<p><span>Not Quite.</span> Different place.</p>" // no comma here
         } // no comma here
     ]
 };
--- project_2/george_trivia/attributes/java/slickQuiz-config.js
+++ project_2/george_trivia/attributes/java/slickQuiz-config.js
@@ -5,68 +5,70 @@
 var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "main":    "<p>What do you know about George Washington? Take this quiz to find out!</p>",
+        "results": "<h5>Learn More</h5><p>How did you do? If you want to learn more about George, just click on the 'Learn More' button!</p>",
+        "level1":  "George Expert",
+        "level2":  "George Fan",
+        "level3":  "Not Bad",
+        "level4":  "Getting there",
+        "level5":  "Give it another shot!" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "Legend has it, George Washington chopped down a/an...",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "apple tree",      "correct": false},
+                {"option": "old woman's house",     "correct": false},
+                {"option": "cherry tree",      "correct": true},
+                {"option": "telephone pole",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> Many believed that he chopped down his father's cherry tree, but later confessed to doing so.</p>",
+            "incorrect": "<p><span>Uhh no.</span> He didn't chop down that.</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+            "q": "George Washington fought in this war as a young man.",
             "a": [
-                {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
-                {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "the French and Indian War",               "correct": true},
+                {"option": "WWII",   "correct": false},
+                {"option": "The Revolutionary War",               "correct": false},
+                {"option": "the War of 1812", "correct": false} // no comma here
             ],
             "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>Nice!</span> George was a commander in the French and Indian War, even at the young age of 23!</p>",
+            "incorrect": "<p><span>Hmmm.</span> You might being thinking of a different time period from when George was a young man.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+            "q": "George Washington is often called by this title.",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "'The Greatest President'",           "correct": false},
+                {"option": "'The Father of our Country'",                  "correct": true},
+                {"option": "'The Coolest President'",  "correct": false},
+                {"option": "'The Brother of our Country'",          "correct": false} // no comma here
             ],
-            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+            "correct": "<p><span>Excellent!</span>He was so important to American History, he is often called the Father of our Country.</p>",
+            "incorrect": "<p><span>Not Quite.</span>That isn't one of his official titles.</p>" // no comma here
         },
         { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+            "q": "George Washington was...",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "The third president",    "correct": false},
+                {"option": "the second president",     "correct": false},
+                {"option": "the ninth president",      "correct": false},
+                {"option": "the first president",   "correct": true} // no comma here
             ],
-            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+            "correct": "<p><span>Nice one!</span> You got it, he was the first president of the United States.</p>",
+            "incorrect": "<p><span>Whoops!</span> Not quite. He wasnt' that president.</p>" // no comma here
         },
         { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+    "q": "George Washinton lived on this plot of land.",
             "a": [
-                {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "Monticello",    "correct": false},
+                {"option": "Washington Farms",     "correct": false},
+                {"option": "Mount Vernon",      "correct": true},
+                {"option": "Ohio",   "correct": false} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+            "correct": "<p><span>Great!</span> Mount Vernon was the name of Washington's estate.</p>",
+            "incorrect": "<p><span> Not quite!</span> His home had a different name.</p>" // no comma here
         } // no comma here
     ]
 };
