53603d855717b78b894e7b94b75fe4648a08f280
ecarlso4 <ecarlso4@nd.edu>
2018-10-25 18:27:19 -0400

    trivia problems

--- project_2/attributes/button.js
+++ project_2/attributes/button.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
 
     /* Toggle On */
     $(".learn_more").click(function () {
-        $(".empty_info").toggleClass("none");
+        $("#slickQuiz").toggleClass("none");
         $(".big_info").toggleClass("visible");
     });
 
     /* Toggle Off */
     $("#back").click(function () {
-        $(".empty_info").toggleClass("none");
+        $("#slickQuiz").toggleClass("none");
         $(".big_info").toggleClass("visible");
     });
 });
